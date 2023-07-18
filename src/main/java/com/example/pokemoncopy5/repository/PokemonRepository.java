ebad9bbbe12162808385fad1e8903084da2e616b
miki <mikias01alemayehu@gmail.com>
2023-07-18 13:13:36 -0400

    k

--- src/main/java/com/example/pokemoncopy5/repository/PokemonRepository.java
+++ src/main/java/com/example/pokemoncopy5/repository/PokemonRepository.java
@@ -4,4 +4,5 @@ import com.example.pokemoncopy5.entity.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PokemonRepository extends JpaRepository<Pokemon,Integer> {
+    Pokemon findByType(String type);
 }
