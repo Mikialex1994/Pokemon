30239e9f821abccf0e4a95d24ebca6a6700447bc
miki <mikias01alemayehu@gmail.com>
2023-07-24 16:17:53 -0400

    test added

--- src/main/java/com/example/pokemoncopy5/dto/PokemonDto.java
+++ src/main/java/com/example/pokemoncopy5/dto/PokemonDto.java
@@ -1,8 +1,14 @@
 package com.example.pokemoncopy5.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class PokemonDto {
 
     private int id;
